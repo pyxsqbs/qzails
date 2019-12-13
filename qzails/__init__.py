2820cf6273524f781fe924274a5518a1d10b0c1e
pyxsqbs <pyxsqbs@163.com>
2019-12-13 16:45:56 +0800

    test

---
+++ qzails/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ Author: qinbaoshuai
+@ Date: 2019-12-13 16:16:43
+@ Email: qinbaoshuai@cloudwalk.cn
+@ LastEditors: qinbaoshuai
+@ LastEditTime: 2019-12-13 16:17:43
+@ Description: __init__.py
+"""
+
+name = 'qzails'
